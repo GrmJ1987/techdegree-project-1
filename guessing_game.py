4493f223ef06b0aa6fa992a8cf8d3ab415cb3336
Jan Grm <grmj12987@gmail.com>
2021-07-21 01:02:34 +0200

    Added script

---
+++ guessing_game.py
@@ -0,0 +1,79 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+For this first project we will be using Workspaces. 
+
+NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+
+"""
+
+import random
+import sys
+
+
+def start_game():
+    """Psuedo-code Hints
+    
+    When the program starts, we want to:
+    ------------------------------------
+    1. Display an intro/welcome message to the player.
+    2. Store a random number as the answer/solution.
+    3. Continuously prompt the player for a guess.
+      a. If the guess greater than the solution, display to the player "It's lower".
+      b. If the guess is less than the solution, display to the player "It's higher".
+    
+    4. Once the guess is correct, stop looping, inform the user they "Got it"
+         and show how many attempts it took them to get the correct number.
+    5. Let the player know the game is ending, or something that indicates the game is over.
+    
+    ( You can add more features/enhancements if you'd like to. )
+    """
+
+    
+    # write your code inside this function.
+    while True:
+        answer = random.randint(1, 10)
+        attempts = 1
+        high_score = []
+        
+        
+        name = input("What is your name?  ")
+        print("""
+        Welcome {}. Today we're going to play a game.
+        I will think about a number between 1 and 10. 
+        You will try to guess it. Of course some additional
+        help and hints will be provided. Best of luck!
+        """.format(name))
+        try:
+            guess = int(input("Please enter your guess: "))
+            if guess < 1 or guess > 10:
+                raise ValueError("Only guesses with range (1-10) are allowed")
+        except ValueError as err:
+            print("Oh no, we ran into an issue. {}. Please try again.".format(err))
+        else:
+            while guess != answer:
+                if guess > answer:
+                    print("It's lower")
+                    guess = int(input("Please enter your guess:  "))
+                    attempts += 1
+                if guess < answer:
+                    print("It's higher")
+                    guess = int(input("Please enter your guess:  "))
+                    attempts += 1
+            print("Got it! The number we were looking for was {}. It took you {} attempts to finish the game".format(answer, attempts))
+            high_score.append(attempts)
+            play_again = input("Well that was fun. I hope you enjoyed it too! Want to play again?  Y/N? ")
+            if play_again.lower() == 'y':
+                print("The current best score is: {}".format(min(high_score)))
+                start_game()
+            else:
+                print("Thanks for hanging with us today {}. Hope to see you soon.".format(name))
+                sys.exit()
+        
+
+
+
+# Kick off the program by calling the start_game function.
+start_game()
